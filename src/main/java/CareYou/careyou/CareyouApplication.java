ef26d011254459eee36feefdbb92b19e6270fd52
shichikuu <shichikuu@outlook.com>
2024-11-28 13:35:17 +0700

    template

---
+++ src/main/java/CareYou/careyou/CareyouApplication.java
@@ -0,0 +1,13 @@
+package CareYou.careyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareyouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CareYou/careyou/CareyouApplicationTests.java
@@ -0,0 +1,13 @@
+package CareYou.careyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
